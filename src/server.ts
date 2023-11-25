c6365943e987aa351397f7aeb58986e65d05aa09
Mitsuyuki Shiiba <bufferings@gmail.com>
2023-11-25 17:25:00 +0900

    Add plugins

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,39 @@
 import Fastify from 'fastify';
+import fp from 'fastify-plugin';
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    top: number;
+    inner: number;
+    innerWithFp: number;
+  }
+}
 
 const fastify = Fastify({
   logger: true,
 });
 
+fastify.decorate('top', 1);
+
 fastify.get('/', async (request, reply) => {
-  return 'Hi!';
+  return { top: fastify.top, inner: fastify.inner, innerWithFp: fastify.innerWithFp };
 });
+
+await fastify.register(async (instance, opts) => {
+  instance.decorate('inner', 2);
+  instance.get('/inner', async (request, reply) => {
+    return { top: instance.top, inner: instance.inner, innerWithFp: instance.innerWithFp };
+  });
+});
+
+await fastify.register(
+  fp(async (instance, opts) => {
+    instance.decorate('innerWithFp', 3);
+    instance.get('/innerWithFp', async (request, reply) => {
+      return { top: instance.top, inner: instance.inner, innerWithFp: instance.innerWithFp };
+    });
+  }),
+);
 
 try {
   await fastify.listen({ port: 3000 });
