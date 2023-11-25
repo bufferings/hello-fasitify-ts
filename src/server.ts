fcd503d076045b7cbe1126becd098725ca648779
Mitsuyuki Shiiba <bufferings@gmail.com>
2023-11-25 11:11:22 +0900

    Add biome

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
-import Fastify from 'fastify'
+import Fastify from "fastify";
 
 const fastify = Fastify({
-  logger: true
-})
+  logger: true,
+});
 
-fastify.get('/', async (request, reply) => {
-  return 'Hi!'
-})
+fastify.get("/", async (request, reply) => {
+  return "Hi!";
+});
 
-await fastify.listen({ port: 3000 })
+try {
+  await fastify.listen({ port: 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
