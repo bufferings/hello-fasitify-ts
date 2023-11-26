e5e734fbb9f065a0534d172198cd70c3b06465f4
Mitsuyuki Shiiba <bufferings@gmail.com>
2023-11-26 09:42:50 +0900

    Check register

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,35 @@
 import Fastify from 'fastify';
 import fp from 'fastify-plugin';
 
-declare module 'fastify' {
-  interface FastifyInstance {
-    top: number;
-    inner: number;
-    innerWithFp: number;
-  }
-}
-
 const fastify = Fastify({
   logger: true,
 });
 
-fastify.decorate('top', 1);
-
 fastify.get('/', async (request, reply) => {
-  return { top: fastify.top, inner: fastify.inner, innerWithFp: fastify.innerWithFp };
+  return { hello: 'Fastify!' };
 });
 
-await fastify.register(async (instance, opts) => {
-  instance.decorate('inner', 2);
-  instance.get('/inner', async (request, reply) => {
-    return { top: instance.top, inner: instance.inner, innerWithFp: instance.innerWithFp };
+await fastify.register(async function myPlugin1(instance, opts) {
+  console.log(`myPlugin1: ${fastify === instance}`);
+  await instance.register(async function myPlugin1_1(instance, opts) {
+    await instance.register(async function myPlugin1_1_1(instance, opts) {});
   });
+  await instance.register(async function myPlugin1_2(instance, opts) {});
 });
 
 await fastify.register(
-  fp(async (instance, opts) => {
-    instance.decorate('innerWithFp', 3);
-    instance.get('/innerWithFp', async (request, reply) => {
-      return { top: instance.top, inner: instance.inner, innerWithFp: instance.innerWithFp };
+  fp(async function myPlugin2_Wrapped(instance, opts) {
+    console.log(`myPlugin2_Wrapped: ${fastify === instance}`);
+    await instance.register(async function myPlugin2_1(instance, opts) {
+      await instance.register(async function myPlugin2_1_1(instance, opts) {});
     });
+    await instance.register(async function myPlugin2_2(instance, opts) {});
   }),
 );
+
+fastify.addHook('onReady', () => {
+  console.log(fastify.printPlugins());
+});
 
 try {
   await fastify.listen({ port: 3000 });
